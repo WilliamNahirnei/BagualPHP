cf9b9b14d9a2f2dd5978ce9160bebf9da59c62df
william <william.lopes@eship.com.br>
2024-08-13 17:59:39 -0300

    testes

--- Src/Modules/Usuario/Controller/UsuarioController.php
+++ Src/Modules/Usuario/Controller/UsuarioController.php
@@ -17,7 +17,7 @@ class UsuarioController {
     /**
      * Lists users with predefined data.
      * 
-     * DOC TESTES aasdasd
+     * DOC TESTES
      * 
      * @return array An array of users, where each user is an instance of stdClass.
      */
